4f6a18d2e019b1cefc9e9843b331aff23cf9f092
Fabrizio Catinella <2322021c@student.gla.ac.uk>
2020-09-26 17:28:43 +0100

    Fix: Wrong line number

--- bracket-debugger.c
+++ bracket-debugger.c
@@ -15,18 +15,17 @@ int main() {
     int ls;
 
     /* Record what line number you are on */
-    int ln = 0;
+    int ln = 1;
 
     /* indivual char */
     char c;  
     int type;
-
+ 
     /* Keep taking in new lines until EOF (0 Characters) or you've ran out of lines to anaylse */
     while ((ls = newline(line, MAXCHARS)) > 0 && ln < (MAXLINES-1)) {
 
         /* Get the character case of each character on line until you reach the end of the string */
         for (int i = 0; (c = line[i]) != '\0'; i++) {
-
             /* if it is a bracket, put it in history */
             if ((type = isopenbracket(c)) != -1) {
                 putbracket(ln, type);
@@ -36,7 +35,7 @@ int main() {
         }
 
         /* On a new line */
-        ln++;
+        ++ln;
     }
 
     printerrors();
--- newline.c
+++ newline.c
@@ -5,7 +5,7 @@ int newline(char line[], int limit) {
     char c;
     int size = 0;
 
-    while (size < (limit-1) && (c = getchar()) != EOF && c != '\n' && c != '^') {
+    while (size < (limit-1) && (c = getchar()) != EOF && c != '\n') {
         line[size] = c;
         ++size;
     }
